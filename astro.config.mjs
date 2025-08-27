5109a3bc0ebc5eda8a4dedbedd8be25130f66403
Ashish jha <ajha5645@gmail.com>
2025-08-27 22:55:22 +0530

    refactor: streamline configuration and enhance global styles integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-  site:"https://ashjha75.github.io/portfolio-web/",
-  // Set base for GitHub Pages deployment
+  site: "https://ashjha75.github.io/portfolio-web/",
+  base: "/portfolio-web/",
+  trailingSlash: "never",
   vite: {
     plugins: [tailwindcss()],
   },
-  
 });
