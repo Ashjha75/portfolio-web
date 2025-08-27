2d188b0bd625d35e927f5c5749ffee5f81e88adc
Ashish jha <ajha5645@gmail.com>
2025-08-27 20:49:09 +0530

    Add GitHub Pages deployment workflow and update configuration for site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import staticAdapter from '@astrojs/adapter-static';
 
-// https://astro.build/config
 export default defineConfig({
-  site:"https://ashjha75.github.io/portfolio-web/",
-  base: '/portfolio/', // Set base for GitHub Pages deployment
+  base: '/portfolio/', // your repo name
+  adapter: staticAdapter(),
   vite: {
     plugins: [tailwindcss()],
   },
