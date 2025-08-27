97fff190985aff696b28b9ba4c47653f35c1c507
Ashish jha <ajha5645@gmail.com>
2025-08-27 20:53:06 +0530

    Update base path in astro.config.mjs for GitHub Pages deployment and modify data-store.json configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-import staticAdapter from '@astrojs/adapter-static';
 
 export default defineConfig({
-  base: '/portfolio/', // your repo name
-  adapter: staticAdapter(),
+
+  base: '/portfolio-web/', // your repo name
   vite: {
     plugins: [tailwindcss()],
   },
