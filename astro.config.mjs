a64471e595e9f89d13d990e922516389b639f6eb
Ashish jha <ajha5645@gmail.com>
2025-08-27 22:23:17 +0530

    chore: update astro dependency and add tailwind configuration
    - Updated astro from version 5.7.12 to 5.13.3 in package.json.
    - Modified global.css to include Tailwind's base, components, and utilities styles.
    - Added a new tailwind.config.cjs file for Tailwind CSS configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site:"https://ashjha75.github.io/portfolio-web/",
-  base: '/portfolio-web/', // Set base for GitHub Pages deployment
+  // Set base for GitHub Pages deployment
   vite: {
     plugins: [tailwindcss()],
   },
