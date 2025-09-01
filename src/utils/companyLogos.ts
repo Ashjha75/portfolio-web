4a086904f49482b0e8ef1026f8aa9bc687139550
Ashish jha <ajha5645@gmail.com>
2025-09-01 17:17:34 +0530

    feat: add company logos utility and update package dependencies for icon integration

---
+++ src/utils/companyLogos.ts
@@ -0,0 +1,18 @@
+// src/utils/companyLogos.ts
+
+export const companyLogos = [
+    // { name: 'Java', icon: 'simple-icons:databricks' },
+    { name: 'Node.js', icon: 'simple-icons:nodedotjs' },
+    { name: 'Spring Boot', icon: 'simple-icons:springboot' },
+    { name: 'Hibernate', icon: 'simple-icons:hibernate' },
+    { name: 'Spring Security', icon: 'simple-icons:springsecurity' },
+    { name: 'Spring Cloud', icon: 'simple-icons:cloudways' },
+    { name: 'MySQL', icon: 'simple-icons:mysql' },
+    { name: 'Redis', icon: 'simple-icons:redis' },
+    { name: 'Git', icon: 'simple-icons:git' },
+    { name: 'Docker', icon: 'simple-icons:docker' },
+    { name: 'Kafka', icon: 'simple-icons:apachekafka' },
+    { name: 'AWS', icon: 'simple-icons:amazonaws' },
+    { name: 'Linux', icon: 'simple-icons:linux' },
+    { name: 'NPM', icon: 'simple-icons:npm' },
+];
